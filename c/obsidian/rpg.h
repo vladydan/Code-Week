b9f2e9d1be7894688637445f5c7bdf4bc740d669
Guibert Timothé <timothe.guibert@outlook.fr>
2014-02-27 11:58:49 +0100

    Create rpg.h

---
+++ c/obsidian/rpg.h
@@ -0,0 +1,10 @@
+#ifndef RPG_H_INCLUDED
+# define RPG_H_INCLUDED
+
+void	my_putchar(char c);
+void	my_putstr(char *str);
+int	printArray(int min, int max, char **array); 
+void	randomArray(int min, int max, char **array);
+
+
+#endif /* RPG_INCLUDED_H_ */
